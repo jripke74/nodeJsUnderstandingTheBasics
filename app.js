609e7cef3865b64ba3158fe4b10f62ee53755a96
jripke74 <jripke74@gmail.com>
2020-07-17 10:57:24 -0700

    log data from req

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
-const server = http.createServer((reg, res) => {
-  console.log(reg);
-  process.exit();
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  // process.exit();
 });
 
 server.listen('5000');
