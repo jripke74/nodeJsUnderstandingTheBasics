4750efb2c01bc77ef67e7365e78a883a6dc21eca
jripke74 <jripke74@gmail.com>
2020-07-19 14:10:11 -0700

    split code into routes

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This always runs!');
-  next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>The "Add Product" Page</h1>')
-});
-
-app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from express!</h1>')
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
