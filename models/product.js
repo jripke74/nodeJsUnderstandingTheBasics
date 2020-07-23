c538ff5dcb2a94241b3ca35bde75ba07d025acc2
jripke74 <jripke74@gmail.com>
2020-07-23 12:03:20 -0700

    add fields to add products

--- models/product.js
+++ models/product.js
@@ -20,8 +20,11 @@ const getProductsFromFile = cb => {
 const products = [];
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.description = description;
   }
 
   save() {
